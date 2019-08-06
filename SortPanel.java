cc559ceffb67df81942c1a4fcdc555c272a97792
Brett Carney <brett.carney1273@gmail.com>
2019-08-06 18:38:32 -0500

    Base setup and todo comments

---
+++ SortPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+
+public class SortPanel extends JPanel
+{
+    private SortAnimationPanel sortAnimationPanel;
+
+    public SortPanel()
+    {
+        // Manage layout for the controls and animation panel
+    }
+
+    // Handle events from buttons
+}
