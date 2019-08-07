d33c597d8034ec7a89c93a3eb2459f9ebe933fbb
Brett Carney <brett.carney1273@gmail.com>
2019-08-06 20:39:39 -0500

    Moved button handling to the App class

--- SortAnimationApp.java
+++ SortAnimationApp.java
@@ -15,6 +15,44 @@ public class SortAnimationApp extends JFrame
         // Sort/Pause/Resume button
     }
 
+    // Handle events from buttons
+    private class HandlePopulateButton implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            // Create an array list with size equal to the panel width
+            ArrayList<Integer> randomizedArray = new ArrayList<Integer>(SortAnimationPanel.this.getWidth());
+
+            // Instantiate randomizer
+            Random rand = new Random();
+
+            // Populate the array with numbers 0 - height of the panel
+            for (Integer thisInt : randomizedArray) 
+            {
+                int randomizedInt = rand.nextInt() % SortAnimationPanel.this.getHeight();
+                randomizedArray.add(randomizedInt);
+            }
+
+            // Convert to an integer array
+            valuesToSort = randomizedArray.toArray();
+            // Call repaint() to display the same array values
+            // Disable populate button
+            // Enable sort button
+        }
+    }
+
+    private class HandleSortButton implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            // Disable sort button
+            // Create a new Thread object from the runnable SortAnimationPanel
+            // Call the Thread object's start() method to start the sort for both panels
+        }
+    }
+
     public static void main()
     {
         SwingUtilities.invokeLater(new Runnable()
--- SortPanel.java
+++ SortPanel.java
@@ -3,55 +3,28 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
 public class SortPanel extends JPanel
 {
     private static final long serialVersionUID = 1L;
+    private final JComboBox algorithmComboBox;
     private SortAnimationPanel sortAnimationPanel;
     private int[] valuesToSort;
 
     public SortPanel()
     {
+        // Create GUI Components
+        algorithmComboBox = new JComboBox<String>(new String[] {"Selection", "Bubble", "Heap"});
+
+        // Instantiate our handlers
+        HandlePopulateButton handlePopulateButton = new HandlePopulateButton();
+        HandleSortButton handleSortButton = new HandleSortButton();
+
+        // Add listeners to the relevant objects
+        algorithmComboBox.add
         // Manage layout for the controls and animation panel
-    }
-
-    // Handle events from buttons
-    private class HandlePopulateButton implements ActionListener
-    {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-            // Create an array list with size equal to the panel width
-            ArrayList<Integer> randomizedArray = new ArrayList<Integer>(SortAnimationPanel.this.getWidth());
-
-            // Instantiate randomizer
-            Random rand = new Random();
-
-            // Populate the array with numbers 0 - height of the panel
-            for (Integer thisInt : randomizedArray) 
-            {
-                int randomizedInt = rand.nextInt() % SortAnimationPanel.this.getHeight();
-                randomizedArray.add(randomizedInt);
-            }
-
-            // Convert to an integer array
-            valuesToSort = randomizedArray.toArray();
-            // Call repaint() to display the same array values
-            // Disable populate button
-            // Enable sort button
-        }
-    }
-
-    private class HandleSortButton implements ActionListener
-    {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-            // Disable sort button
-            // Create a new Thread object from the runnable SortAnimationPanel
-            // Call the Thread object's start() method to start the sort for both panels
-        }
     }
 
     private class SortAnimationPanel extends JPanel implements Runnable
