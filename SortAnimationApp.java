8f8c11e45af1e0824483bb68e10c6732f3c70fe6
Maxwell Oury <robertoury97@gmail.com>
2019-08-06 23:52:36 -0500

    started graphical processing and added basic sort

--- SortAnimationApp.java
+++ SortAnimationApp.java
@@ -2,19 +2,46 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import java.awt.event.ActionListener;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import java.awt.GridBagConstraints;
+import java.awt.Component;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.lang.Math;
+
 
 public class SortAnimationApp extends JFrame
 {
+
     private static final long serialVersionUID = 1L;
     private SortPanel panel1;
     private SortPanel panel2;
+    private JPanel controlPanel;
     private final JButton populateButton;
     private final JComboBox sortSpeedComboBox;
     private final JButton actionButton;
+    private final GridBagLayout layout; // layout of this frame
+    private final GridBagConstraints constraints; // layout's constraints
 
     public SortAnimationApp(String title) 
     {
         super(title);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 600); 
+        setVisible(true); 
 
         // Setup Layout and Constraints
         layout = new GridBagLayout();
@@ -25,12 +52,18 @@ public class SortAnimationApp extends JFrame
         populateButton = new JButton("Populate");
         sortSpeedComboBox = new JComboBox<String>(new String[] {"Slow", "Medium", "Fast"});
         actionButton = new JButton("Sort");
-
-        // Add components
-        addComponent(populateButton, 3, 2, 1, 1);
-        addComponent(sortSpeedComboBox, 4, 2, 1, 1);
-        addComponent(actionButton, 5, 2, 1, 1);
-            // Add Two SortPanel objects to the app layout
+    
+        controlPanel = new JPanel();
+        controlPanel.add(populateButton);
+        controlPanel.add(actionButton);
+    
+    
+        panel1 = new SortPanel();
+        panel2 = new SortPanel();
+        addComponent(panel1, 0, 0, 1, 1, 1, 1);
+        addComponent(panel2, 0, 1, 1, 1, 1, 1);
+        addComponent(controlPanel,1 ,0 ,2 ,1 , 0, 0);
+    
 
         // Instantiate Handlers
         HandlePopulateButton handlePopulateButton = new HandlePopulateButton();
@@ -48,26 +81,14 @@ public class SortAnimationApp extends JFrame
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            // Create an array list with size equal to the panel width
-            ArrayList<Integer> randomizedArray = new ArrayList<Integer>(SortAnimationPanel.this.getWidth());
-
-            // Instantiate randomizer
-            Random rand = new Random();
-
-            // Populate the array with numbers 0 - height of the panel
-            for (Integer thisInt : randomizedArray) 
-            {
-                int randomizedInt = rand.nextInt() % SortAnimationPanel.this.getHeight();
-                randomizedArray.add(randomizedInt);
-            }
-
-            // Convert to an integer array
-            valuesToSort = randomizedArray.toArray();
-
+            long seed = System.currentTimeMillis();
+            panel1.generateArray(seed);
+            panel2.generateArray(seed);
             // Call repaint() to display the same array values
-
+            panel1.repaint();
+            panel2.repaint();
             // Disable populate button
-            populateButton.setEnabled(false);
+            //populateButton.setEnabled(false);
 
             // Enable action button as sort
             actionButton.setEnabled(true);
@@ -81,34 +102,38 @@ public class SortAnimationApp extends JFrame
         public void actionPerformed(ActionEvent e)
         {
             // Disable sort button
-            actionButton.setEnabled(false);
-            
+            //actionButton.setEnabled(false);
+            panel1.startsort();
+            panel2.startsort();
             // Create a new Thread object from the runnable SortAnimationPanel
             // Call the Thread object's start() method to start the sort for both panels
         }
     }
 
-    // Method to set constraints on 
+    // method to set constraints on 
     private void addComponent(Component component,
-    int row, int column, int width, int height)
-    {
-       constraints.gridx = column; 
-       constraints.gridy = row;
-       constraints.gridwidth = width;
-       constraints.gridheight = height;
-       layout.setConstraints(component, constraints); // Set constraints
-       add(component); // Add component
-    }
+    int row, int column, int width, int height, double weightx, double weighty)
+ {
+    constraints.gridx = column; 
+    constraints.gridy = row;
+    constraints.gridwidth = width;
+    constraints.gridheight = height;
+    constraints.weightx = weightx;
+    constraints.weighty = weighty;
+    constraints.fill = GridBagConstraints.BOTH;
+    layout.setConstraints(component, constraints); // set constraints
+    add(component); // add component
+ }
 
     // Note to max: I'm not yet positive how we start our main based on the document's instructions, but I think we need it in here
-    public static void main()
+    public static void main(String[] args)
     {
         SwingUtilities.invokeLater(new Runnable()
         {
             @Override
             public void run()
             {
-                new SortAnimationApp();
+                new SortAnimationApp("Testing");
             }
         });
     }    
--- SortPanel.java
+++ SortPanel.java
@@ -4,46 +4,129 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import java.awt.GridBagConstraints;
+import java.awt.Component;
+import java.awt.Graphics;
 
-public class SortPanel extends JPanel
-{
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.util.Random;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.lang.Math;
+
+public class SortPanel extends JPanel {
     private static final long serialVersionUID = 1L;
     private final JComboBox algorithmComboBox;
     private SortAnimationPanel sortAnimationPanel;
+    private Border blackline = BorderFactory.createLineBorder(Color.black);
+    private Border thicc = BorderFactory.createLineBorder(Color.lightGray, 5);
     private int[] valuesToSort;
+    private GridBagConstraints constraints; // layout's constraints
+    private GridBagLayout layout; // layout of this frame
 
-    public SortPanel()
-    {
+    public SortPanel() {
         // Setup Layout and Constraints
         layout = new GridBagLayout();
-        setLayout(layout);      
+        setLayout(layout);
         constraints = new GridBagConstraints();
-        
+
+        setBackground(Color.lightGray);
+        setBorder(thicc);
+
+        sortAnimationPanel = new SortAnimationPanel();
+
         // Create GUI Components
-        algorithmComboBox = new JComboBox<String>(new String[] {"Selection", "Bubble", "Heap"});
+        algorithmComboBox = new JComboBox<String>(new String[] { "Selection", "Bubble", "Heap" });
+
+        addComponent(sortAnimationPanel, 0, 0, 1, 1, 1, 1);
+        addComponent(algorithmComboBox, 1, 0, 1, 1, 0, 0);
 
         // Manage layout for the controls and animation panel
     }
 
-    private class SortAnimationPanel extends JPanel implements Runnable
+    public void generateArray(long seed) {
+        // Create an array list with size equal to the panel width
+        // SortAnimationPanel.this.getHeight()
+        valuesToSort = new int[getWidth()];
+
+        // Instantiate randomizer
+        Random rand = new Random();
+
+        rand.setSeed(seed);
+
+        // Populate the array with numbers 0 - height of the panel
+        for (int i = 0; i < valuesToSort.length; i++) {
+            int randomizedInt = Math.abs(rand.nextInt()) % getHeight();
+            valuesToSort[i] = randomizedInt;
+        }
+
+        rand.setSeed(1);
+    }
+
+    //starting the sorting threads
+    public void startsort() {
+        Thread t0 = new Thread(sortAnimationPanel);
+        t0.start();
+    }
+
+
+    public void BubbleSort()
     {
+        try {
+            boolean swapped = true;
+            int j = 0;
+            int tmp;
+            while (swapped) {
+                swapped = false;
+                j++;
+                for (int i = 0; i < valuesToSort.length - j; i++) {
+                    if (valuesToSort[i] > valuesToSort[i + 1]) {
+                        tmp = valuesToSort[i];
+                        valuesToSort[i] = valuesToSort[i + 1];
+                        valuesToSort[i + 1] = tmp;
+                        swapped = true;
+                        repaint();
+                        Thread.sleep(1);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            // Displays that the thread was interupted.
+            System.out.println(Thread.currentThread().getName() + " interrupted");
+        }
+    }
+
+    private class SortAnimationPanel extends JPanel implements Runnable {
         private static final long serialVersionUID = 1L;
 
-        SortAnimationPanel(String title)
-        {
-            super(title);
+        SortAnimationPanel() {
+            repaint();
+        }
 
-            // Get dimmensions of panel and clear it
-
-            if (valuesToSort != null)
-            {
+        public void paintComponent(Graphics g) {
+            setBackground(Color.white);
+            if (valuesToSort != null) {
                 // Draw lines representing values
+                super.paintComponent(g);
+                g.setColor(Color.blue);
+                for (int i = 0; i < valuesToSort.length; i++) {
+                    g.fillRect(i, getHeight() - valuesToSort[i], 1, valuesToSort[i]);
+                }
             }
-            
-        }        
+        }
 
-        public void run()
-        {
+        public void run() {
+            //Testing Basic Sorting
+            BubbleSort();
             // Call appropriate sort method utilizing the combo box to sort in asc. order
             // Call repaint() everytime there's a swap
             // After each pass through an outer loop, sleep the thread for 100 miliseconds
@@ -51,15 +134,17 @@ public class SortPanel extends JPanel
         // Override the paintComponent() method
     }
 
-    // Method to set constraints on 
-    private void addComponent(Component component,
-    int row, int column, int width, int height)
-    {
-       constraints.gridx = column; 
-       constraints.gridy = row;
-       constraints.gridwidth = width;
-       constraints.gridheight = height;
-       layout.setConstraints(component, constraints); // Set constraints
-       add(component); // Add component
+    // method to set constraints on
+    private void addComponent(Component component, int row, int column, int width, int height, int weightx,
+            int weighty) {
+        constraints.gridx = column;
+        constraints.gridy = row;
+        constraints.gridwidth = width;
+        constraints.gridheight = height;
+        constraints.weightx = weightx;
+        constraints.weighty = weighty;
+        constraints.fill = GridBagConstraints.BOTH;
+        layout.setConstraints(component, constraints); // set constraints
+        add(component); // add component
     }
 }
